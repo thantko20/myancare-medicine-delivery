b41f0cd184171303b40241373ea8cc637c2e99c4
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-16 20:09:03 +0630

    Log error stack if in development

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
 const { NODE_ENV } = require('../constants');
 const ApiError = require('../utils/apiError');
 
 function sendApiError(error, res) {
+  if (NODE_ENV !== 'production') console.log(error);
   res.status(error.statusCode).json({
     code: error.statusCode,
     message: error.message,
@@ -10,6 +12,7 @@ function sendApiError(error, res) {
 }
 
 function sendError(error, res) {
+  console.error(error);
   res.status(500).json({
     code: 500,
     message:
