dafdba7f3dcb4c85721d83f64d88348ef18ba01c
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-17 21:08:37 +0630

    Fix:Function returning number not element

--- src/utils/getRandomElementFromArray.js
+++ src/utils/getRandomElementFromArray.js
@@ -1,6 +1,6 @@
 function getRandomElementFromArray(array) {
   const length = array.length;
-  return Math.floor(Math.random() * length);
+  return array[Math.floor(Math.random() * length)];
 }
 
 module.exports = getRandomElementFromArray;
