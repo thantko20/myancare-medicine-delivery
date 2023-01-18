c3f90c693c4cd4a3fc81f05aae9bb5bb7f17cfde
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-18 21:49:28 +0630

    Add basic user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,8 +3,17 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  customerId: { type: String, unique: true },
+  phone: { type: String, unique: true, required: true },
+  address: {
+    zipcode: { type: String, required: true },
+    street: { type: String, required: true },
+    state: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
 });
 
 const hashPassword = async function (next) {
@@ -15,6 +24,13 @@ const hashPassword = async function (next) {
 };
 
 userSchema.pre('save', hashPassword);
+userSchema.pre('save', async function (next) {
+  if (!this.isNew) return next();
+
+  const docCount = await this.constructor.count({});
+  this.customerId = `C-${(docCount + 1).toString().padStart(4, '0')}`;
+  next();
+});
 
 const userModel = model('User', userSchema);
 
