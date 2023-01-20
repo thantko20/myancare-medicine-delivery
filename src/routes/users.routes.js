c26b674bae64e5cd7dbfcae23ccd235df9406166
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-20 23:35:18 +0630

    Add authenticate middleware to get route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/users.controller');
+const authenticate = require('../middlewares/authenticate');
+const {
+  restrictUserTypes,
+  restrictAdmins,
+} = require('../middlewares/authorize');
 
-router.get('/', userController.getAllUsers);
+router.get(
+  '/',
+  authenticate,
+  restrictUserTypes('admin'),
+  restrictAdmins(),
+  userController.getAllUsers
+);
 
 module.exports = router;
