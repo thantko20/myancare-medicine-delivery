028df1b639f40d51aeef5ee90c0e184f1767a2ea
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-23 09:28:15 +0630

    Add route for getting current user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const userService = require('../services/users.service');
+const ApiError = require('../utils/apiError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -11,3 +12,12 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     count: users.length,
   });
 });
+
+exports.getMe = (req, res, next) => {
+  if (!req.user) return next(ApiError.badRequest());
+
+  res.json({
+    code: 200,
+    data: req.user,
+  });
+};
