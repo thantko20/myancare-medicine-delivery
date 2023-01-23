006ababac315839aee9f1af19a5e8a15db39bfa0
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-23 10:13:06 +0630

    Add dbname to mongoose connect options

--- src/data/importData.js
+++ src/data/importData.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { MONGODB_URI } = require('../constants');
 const Medicine = require('../models/medicine.model');
+const Category = require('../models/category.model');
 const mongoose = require('mongoose');
 const fs = require('fs');
 
@@ -8,15 +9,25 @@ const medicines = JSON.parse(
   fs.readFileSync(`${__dirname}/medicines.json`, 'utf-8')
 );
 
+const categories = JSON.parse(
+  fs.readFileSync(`${__dirname}/categories.json`, 'utf-8')
+);
+
 const start = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
-    await Medicine.deleteMany();
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(MONGODB_URI, {
+      dbName: 'medicine-delivery',
+    });
+    await Category.deleteMany({});
+    await Medicine.deleteMany({});
     console.log('Deleted well');
-    await Medicine.create(medicines);
+    await Category.insertMany(categories);
+    await Medicine.insertMany(medicines);
     console.log('Data are imported successfully!');
     process.exit(0);
   } catch (err) {
+    await mongoose.disconnect();
     console.log(err);
     process.exit(1);
   }
