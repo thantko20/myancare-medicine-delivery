4f249cff7913f8c19f3061a414424878fbb1d0a0
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-23 09:28:15 +0630

    Add route for getting current user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { USER_TYPES } = require('../constants');
 const userController = require('../controllers/users.controller');
 const authenticate = require('../middlewares/authenticate');
 const {
@@ -10,9 +11,16 @@ const {
 router.get(
   '/',
   authenticate,
-  restrictUserTypes('admin'),
+  restrictUserTypes(USER_TYPES.admin),
   restrictAdmins(),
   userController.getAllUsers
+);
+
+router.get(
+  '/me',
+  authenticate,
+  restrictUserTypes(USER_TYPES.customer),
+  userController.getMe
 );
 
 module.exports = router;
